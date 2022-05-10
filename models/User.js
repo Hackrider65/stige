a88b023cb8534b088e59737d848287ee7e0f6f10
roopesh k <roopeshkarthik62@gmail.com>
2022-05-10 18:59:07 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    phone: Number,
+    email: String,
+    photo:
+	{
+		data: Buffer,
+		contentType: String
+	}
+});
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", UserSchema);
+
